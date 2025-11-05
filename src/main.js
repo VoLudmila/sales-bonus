6fe051a080ff1ca76d4f183f05529c27872ca7b7
GitLudmila <hunter415890@mail.ru>
2025-11-05 19:16:52 +0300

    First Commit

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@
  * @returns {number}
  */
 function calculateSimpleRevenue(purchase, _product) {
-   // @TODO: Расчет выручки от операции
+    // @TODO: Расчет выручки от операции
+    const discount = 1 - (purchase.discount / 100);
+    return purchase.sale_price * purchase.quantity * discount;
 }
 
 /**
@@ -17,6 +19,17 @@ function calculateSimpleRevenue(purchase, _product) {
  */
 function calculateBonusByProfit(index, total, seller) {
     // @TODO: Расчет бонуса от позиции в рейтинге
+    const { profit } = seller;
+    let bonusPercent = 0.05;
+    if (index == 0) {
+        bonusPercent = 0.15;
+    } else if (index == 1 || index == 2) {
+        bonusPercent = 0.1;
+    } else if (index == total - 1) {
+        bonusPercent = 0;
+    }
+
+    return profit * bonusPercent;
 }
 
 /**
@@ -27,18 +40,81 @@ function calculateBonusByProfit(index, total, seller) {
  */
 function analyzeSalesData(data, options) {
     // @TODO: Проверка входных данных
+    if (!data
+        || !Array.isArray(data.sellers)
+        || !Array.isArray(data.products)
+        || !Array.isArray(data.purchase_records)
+        || data.sellers.length === 0
+        || data.products.length === 0
+        || data.purchase_records.length === 0
+    ) {
+        throw new Error('Некорректные входные данные');
+    }
 
     // @TODO: Проверка наличия опций
+    const { calculateRevenue, calculateBonus } = options;
+    if (!calculateRevenue || !calculateBonus) {
+        throw new Error('ГДЕ ОПЦИИ, БИЛЛИ!?');
+    }
 
-    // @TODO: Подготовка промежуточных данных для сбора статистики
+    // Step 3 ???
+    const sellerStats = data.sellers.map(seller => {
+        return {
+            "id": seller.id,
+            "first_name": seller.first_name,
+            "last_name": seller.last_name,
+            "start_date": seller.start_date,
+            "position": seller.position
+        }
+    });
 
-    // @TODO: Индексация продавцов и товаров для быстрого доступа
+    const sellerIndex = Object.fromEntries(sellerStats.map(seller => [seller.id, seller]));
+    const productIndex = Object.fromEntries(data.products.map(product => [product.sku, product]));
 
     // @TODO: Расчет выручки и прибыли для каждого продавца
+    data.purchase_records.forEach(record => {
+        const seller = sellerIndex[record.seller_id];
+        seller.sales_count += 1;
+        seller.revenue += record.total_amount;
+
+        record.items.forEach(item => {
+            const product = productIndex[item.sku];
+            const cost = product.purchase_price * item.quantity;
+            const revenue = calculateRevenue(item, product);
+            const profit = revenue - cost;
+
+            seller.profit += profit;
+
+            if (!seller.products_sold[item.sku]) {
+                seller.products_sold[item.sku] = 0;
+            }
+            seller.products_sold[item.sku] += item.quantity;
+        });
+    });
 
     // @TODO: Сортировка продавцов по прибыли
 
+
     // @TODO: Назначение премий на основе ранжирования
+    let total = 0;
+    for (let key in sellerStats) { total++; }
+
+    sellerStats.forEach((seller, index) => {
+        seller.bonus = calculateBonusByProfit(index, total, seller)
+        seller.top_products = Object.entries(seller.products_sold)
+            .map(([sku, quantity]) => ({ sku, quantity })) // сократил до 1 строчки путем деструктуризации в аргументах
+            .sort((a, b) => b.quantity - a.quantity) // сортируем с мутированием текущего массива
+            .slice(0, 10); // обрезаем
+    });
 
     // @TODO: Подготовка итоговой коллекции с нужными полями
+    return sellerStats.map(seller => ({
+        seller_id: seller.seller_id, // Строка, идентификатор продавца
+        name: seller.name, // Строка, имя продавца
+        revenue: +seller.revenue.toFixed(2),// Число с двумя знаками после точки, выручка продавца
+        profit: +seller.profit.toFixed(2),// Число с двумя знаками после точки, прибыль продавца
+        sales_count: seller.sales_count,// Целое число, количество продаж продавца
+        top_products: seller.top_products,// Массив объектов вида: { "sku": "SKU_008","quantity": 10}, топ-10 товаров продавца
+        bonus: +seller.bonus.toFixed(2)// Число с двумя знаками после точки, бонус продавца
+    }));
 }
